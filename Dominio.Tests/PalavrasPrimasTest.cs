c622604915c5bcd28a3574b0b869b5156e2128ae
Guilherme <cavalcante.guilherme91@gmail.com>
2019-01-15 08:23:47 -0300

    ISS - TDD Criação da classe de Testes PalavrasPrimasTest. Teste criado: "DeveRetornar1ParaAMinusculo". Foi criada a classe PalavraPrima: O construtor de PalavraPrima pode receber uma string que já aceitará seu atributo palavra; Foi criado o método soma da palavra.

---
+++ Dominio.Tests/PalavrasPrimasTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Dominio;
+
+namespace Dominio.Tests
+{
+    class PalavrasPrimasTest
+    {
+        [Test]
+        public void DeveRetornar1ParaAMinusculo()
+        {
+            var palavraPrima = new PalavraPrima("a");
+            int resultado = palavraPrima.calculaSomaDaPalavra();
+            Assert.AreEqual(1, resultado);
+        }
+    }
+}
---
+++ Dominio/PalavraPrima.cs
@@ -0,0 +1,17 @@
+﻿namespace Dominio
+{
+    public class PalavraPrima
+    {
+        private string palavra;
+
+        public PalavraPrima(string palavra)
+        {
+            this.palavra = palavra;
+        }
+
+        public int calculaSomaDaPalavra()
+        {
+            return 1;
+        }
+    }
+}
